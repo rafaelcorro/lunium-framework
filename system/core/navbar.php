4f760df94397410e4726bddb0aa7de0c6338ed60
rafaelcorro <rafaelcorro@gmail.com>
2020-07-19 17:51:53 +0200

    The menu is now hidden when the user has not logged in

--- system/core/navbar.php
+++ system/core/navbar.php
@@ -47,43 +47,47 @@ class Navbar{
         //cargamos el array de configuración del menú
         require_once '../themes/'. constant('TEMPLATE').'/config.php';
         
-        //pintamos el menú
-        $li_attr=$menuAttr["li-menu"];
-        $a_attr=$menuAttr["a-menu"];
-        $menu="<ul ".$menuAttr["ul-menu"].">";
-        foreach($parents as $value){
-            if($value!="none"){
-                //li-parent
-                $menu=$menu."<li ".$menuAttr["li-parent"].">";
-                $menu=$menu."<a ".$menuAttr["a-parent"]." href=''>".$value."</a>";
-                $menu=$menu."<ul ".$menuAttr["ul-submenu"].">";
-                $li_attr=$menuAttr["li-submenu"];
-                $a_attr=$menuAttr["a-submenu"];
-            }
-            foreach ($items as $value2){
-                if($value==$value2["parent"] && $value2["title"]!="none"){
-                    $menu=$menu."<li ".$li_attr."><a ".$a_attr." href='/".constant('DIRECTORY')."/public/".$value2["modulo"]."/'>".$value2["title"]."</a></li>";
-                } 
-            }
-            if($value!="none"){
-                $menu=$menu."</ul></li>";
-                $li_attr=$menuAttr["li-menu"];
-                $a_attr=$menuAttr["a-menu"];
-            }
-        }
+        
         //--------------login--------------
         if(isset($_SESSION["authorized"]) && $_SESSION["authorized"]==1){
-            
+
+            //pintamos el menú
+            $li_attr=$menuAttr["li-menu"];
+            $a_attr=$menuAttr["a-menu"];
+            $menu="<ul ".$menuAttr["ul-menu"].">";
+            foreach($parents as $value){
+                if($value!="none"){
+                    //li-parent
+                    $menu=$menu."<li ".$menuAttr["li-parent"].">";
+                    $menu=$menu."<a ".$menuAttr["a-parent"]." href=''>".$value."</a>";
+                    $menu=$menu."<ul ".$menuAttr["ul-submenu"].">";
+                    $li_attr=$menuAttr["li-submenu"];
+                    $a_attr=$menuAttr["a-submenu"];
+                }
+                foreach ($items as $value2){
+                    if($value==$value2["parent"] && $value2["title"]!="none"){
+                        $menu=$menu."<li ".$li_attr."><a ".$a_attr." href='/".constant('DIRECTORY')."/public/".$value2["modulo"]."/'>".$value2["title"]."</a></li>";
+                    } 
+                }
+                if($value!="none"){
+                    $menu=$menu."</ul></li>";
+                    $li_attr=$menuAttr["li-menu"];
+                    $a_attr=$menuAttr["a-menu"];
+                }
+            }
+
+            //menu session
             $menu=$menu."<li ".$menuAttr["li-parent"]."><a ".$menuAttr["a-parent"]." href=''>".$_SESSION["login_name"]."</a>";
             $menu=$menu."<ul ".$menuAttr["ul-submenu"].">";
             $menu=$menu."<li ".$menuAttr["li-submenu"].">";
             $menu=$menu."<a ".$menuAttr["a-submenu"]." href='/".constant('DIRECTORY')."/public/login/logout/'>";
             $menu=$menu."<i class='fas fa-user'></i>Cerrar sessión</a></li>";
             $menu=$menu."</ul></li>";
+
+            //---------------------------------
+            $menu=$menu."</ul>";
+            
+            return $menu;
         }
-        //---------------------------------
-        $menu=$menu."</ul>";
-        
-        return $menu;
     }
 }
